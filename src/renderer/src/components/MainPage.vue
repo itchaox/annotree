c3bcb09718471909902129b60fd08c3cb2b16e88
itchaox <1462831573@qq.com>
2024-07-30 00:07:47 +0800

    feat: Add download tree img

--- src/renderer/src/components/MainPage.vue
+++ src/renderer/src/components/MainPage.vue
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2024-07-06 11:57
  * @LastAuthor : itchaox
- * @LastTime   : 2024-07-29 23:01
+ * @LastTime   : 2024-07-30 00:05
  * @desc       :
 -->
 <script setup lang="ts">
@@ -822,13 +822,29 @@ function copyImg() {}
 // 导出图片
 function exportImg() {
   html2canvas(document.querySelector('#capture'), {
-    scrollX: 0,
-    scrollY: 0
+    backgroundColor: '#f8f9fa',
+    useCORS: true, //支持图片跨域
+    scale: 1 //设置放大的倍数
   }).then((canvas) => {
+    // 获取当前时间
+    const now = new Date()
+    const year = now.getFullYear()
+    const month = String(now.getMonth() + 1).padStart(2, '0') // 月份从0开始，所以加1，并确保格式为两位数
+    const day = String(now.getDate()).padStart(2, '0')
+    const hours = String(now.getHours()).padStart(2, '0')
+    const minutes = String(now.getMinutes()).padStart(2, '0')
+    const seconds = String(now.getSeconds()).padStart(2, '0')
+
+    // 格式化时间字符串
+    const formattedDate = `${year}-${month}-${day} ${hours}-${minutes}-${seconds}`
+
+    // 设置下载文件名
+    const filename = `Annotree ${formattedDate}.png`
+
     const img = canvas.toDataURL('image/png')
     const link = document.createElement('a')
     link.href = img
-    link.download = 'screenshot.png'
+    link.download = filename
     link.click()
   })
 }
@@ -997,12 +1013,13 @@ function exportImg() {
         </div>
 
         <div
-          id="capture"
           class="tree-scroller"
           ref="scrollRight"
           @scroll="handleScroll(scrollRight, scrollLeft)"
         >
-          <pre class="tree-node" v-for="item in previewList" :key="item.id">{{ item.value }}</pre>
+          <div id="capture">
+            <pre class="tree-node" v-for="item in previewList" :key="item.id">{{ item.value }}</pre>
+          </div>
         </div>
       </div>
 
